5867e38b2372dfed7b8c08081515f36bce7ccc57
Marc <brout.marc@gmail.com>
2016-02-28 15:33:09 +0000

    makefile interface working

--- include/game.h
+++ include/game.h
@@ -5,7 +5,7 @@
 ** Login   <duhieu_b@epitech.net>
 **
 ** Started on  Sun Feb 28 12:01:44 2016 benjamin duhieu
-** Last update Sun Feb 28 13:12:27 2016 marc brout
+** Last update Sun Feb 28 14:48:33 2016 marc brout
 */
 
 #ifndef GAME_H_
@@ -14,19 +14,34 @@
 # define HEIGHT 768
 
 # include "lapin.h"
+# incude "parser.h"
 
-typedef	struct	s_image
+typedef struct		s_list
+{
+  int			**avail;
+  int			position;
+  int			x;
+  int			y;
+  int			cube;
+  int			value;
+  struct s_list		*next;
+  struct s_list		*prev;
+}			t_list;
+
+typedef	struct		s_image
 {
   t_bunny_pixelarray	*grid;
   t_bunny_pixelarray	*number[9];
-}		t_image;
+}			t_image;
 
-typedef struct	s_main
+typedef struct		s_main
 {
   t_bunny_pixelarray	*pix;
   t_bunny_window	*win;
   t_image		nbr;
-}		t_main;
+  t_square		*sudoki;
+  int			first_game;
+}			t_main;
 
 /*
 ** Init graphics main : main.c
